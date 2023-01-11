f4811f369b9d1d4368ab5689ebf7a0266eced314
Bonface Masira <bonfacemasira@gmail.com>
2023-01-11 16:13:17 +0300

    Done

--- src/reducer.js
+++ src/reducer.js
@@ -10,4 +10,27 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+// state = changeState(state, action);
+// console.log(state)
+// state = changeState(state, action);
+// console.log(state)
+// state = changeState(state, action);
+// console.log(state)
+
+function render() {
+  document.body.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+  return state;
+}
+
+console.log(dispatch({ type: "counter/increment"}));
+
+console.log(dispatch({ type: "counter/increment"}));
+
+console.log(dispatch({ type: "counter/increment"}));
+
+console.log(dispatch({ type: "counter/increment"}));
